26004ae5238725ac8720ec98efce29d648728b13
Mikhail Kleymenov <mikhail@meetup.com>
2024-04-15 22:49:00 +0200

    Merchant webhook

--- server/src/main/java/com/dataart/itkonekt/controller/WebhookController.java
+++ server/src/main/java/com/dataart/itkonekt/controller/WebhookController.java
@@ -1,7 +1,84 @@
 package com.dataart.itkonekt.controller;
 
+import com.dataart.itkonekt.entity.Merchant;
+import com.dataart.itkonekt.repository.MerchantRepository;
+import com.dataart.itkonekt.stripe.StripeApi;
+import com.stripe.model.Account;
+import com.stripe.model.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
+@RequestMapping(consumes = "application/json", produces = "application/json")
 public class WebhookController {
+  private static final Logger LOG = LoggerFactory.getLogger(WebhookController.class);
+
+  private final StripeApi stripeApi;
+  private final MerchantRepository merchantRepository;
+
+  public WebhookController(@Autowired StripeApi stripeApi, @Autowired MerchantRepository merchantRepository) {
+    this.stripeApi = stripeApi;
+    this.merchantRepository = merchantRepository;
+  }
+
+  @PostMapping("/webhook")
+  public ResponseEntity<?> webhook(HttpEntity<String> request) {
+    var payload = request.getBody();
+    var signature = request.getHeaders().getFirst("Stripe-Signature");
+
+    return stripeApi.verifyEvent(payload, signature)
+        .filter(this::processEvent)
+        .map(__ -> ResponseEntity.ok().build())
+        .orElseGet(() -> ResponseEntity.internalServerError().build());
+  }
+
+  private boolean processEvent(Event event) {
+    LOG.info("Received event of type: {}", event.getType());
+
+    return switch (event.getType()) {
+      // Connect events
+      case "account.updated" ->
+          StripeApi.deserializeEventObject(event, Account.class).stream().anyMatch(this::onAccountUpdated);
+
+      default -> {
+        LOG.info("No handler for event of type: {}", event.getType());
+        yield true;
+      }
+    };
+  }
+
+  private boolean onAccountUpdated(Account account) {
+    return Optional.ofNullable(account.getMetadata())
+        .map(metadata -> metadata.get("merchant_id"))
+        .map(Integer::parseInt)
+        .flatMap(merchantRepository::findById)
+        .map(merchant -> {
+          merchant.setStatus(getMerchantStatus(account));
+          return merchantRepository.save(merchant);
+        })
+        .isPresent();
+  }
+
+  private static Merchant.Status getMerchantStatus(Account account) {
+    var disabledReason = Optional.ofNullable(account.getRequirements())
+        .map(Account.Requirements::getDisabledReason);
+    if (disabledReason.stream().anyMatch(reason -> reason.contains("rejected"))) {
+      return Merchant.Status.REJECTED;
+    }
+    if (disabledReason.stream().anyMatch("under_review"::equals)) {
+      return Merchant.Status.IN_REVIEW;
+    }
+    if (!account.getDetailsSubmitted() || !account.getChargesEnabled()) {
+      return Merchant.Status.PENDING;
+    }
+    return Merchant.Status.ACTIVE;
+  }
 }
--- server/src/main/java/com/dataart/itkonekt/stripe/StripeApi.java
+++ server/src/main/java/com/dataart/itkonekt/stripe/StripeApi.java
@@ -1,11 +1,15 @@
 package com.dataart.itkonekt.stripe;
 
 import com.dataart.itkonekt.entity.Merchant;
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Account;
 import com.stripe.model.AccountLink;
+import com.stripe.model.Event;
 import com.stripe.model.LoginLink;
+import com.stripe.model.StripeObject;
 import com.stripe.net.RequestOptions;
+import com.stripe.net.Webhook;
 import com.stripe.param.AccountCreateParams;
 import com.stripe.param.AccountLinkCreateParams;
 import com.stripe.param.LoginLinkCreateOnAccountParams;
@@ -21,9 +25,12 @@ public class StripeApi {
   private static final Logger LOG = LoggerFactory.getLogger(StripeApi.class);
 
   private final RequestOptions requestOptions;
+  private final String webhookSecret;
 
-  public StripeApi(@Value("#{environment['STRIPE_API_KEY']}") String apiKey) {
+  public StripeApi(@Value("#{environment['STRIPE_API_KEY']}") String apiKey,
+                   @Value("#{environment['STRIPE_WEBHOOK_SECRET']}") String webhookSecret) {
     this.requestOptions = RequestOptions.builder().setApiKey(apiKey).build();
+    this.webhookSecret = webhookSecret;
   }
 
   public Optional<String> createConnectAccount(Merchant merchant) {
@@ -72,5 +79,28 @@ public class StripeApi {
       LOG.error("Couldn't create login link for Stripe account {}", stripeAccountId, ex);
       return Optional.empty();
     }
+  }
+
+  public Optional<Event> verifyEvent(String payload, String signature) {
+    try {
+      return Optional.ofNullable(Webhook.constructEvent(payload, signature, webhookSecret));
+    } catch (StripeException ex) {
+      LOG.error("Couldn't verify Stripe event '{}'. Signature: '{}'", payload, signature, ex);
+      return Optional.empty();
+    }
+  }
+
+  public static <T extends StripeObject> Optional<T> deserializeEventObject(Event event, Class<T> objectClass) {
+    var deserializer = event.getDataObjectDeserializer();
+    return deserializer.getObject().or(() -> {
+      try {
+        return Optional.of(deserializer.deserializeUnsafe());
+      } catch (StripeException ex) {
+        LOG.error("Couldn't deserialize object of event {} due to Stripe API version mismatch. Event API version: {}," +
+                  " Stripe SDK API version: {}",
+            event.getId(), event.getApiVersion(), Stripe.API_VERSION);
+        return Optional.empty();
+      }
+    }).map(objectClass::cast);
   }
 }
